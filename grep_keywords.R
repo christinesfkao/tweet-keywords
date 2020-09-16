eeb6fa8fb99adabd1661295a6058ac3abea61921
高紹芳｜Christine Shào-Fāng Kāo <christinesfkao@gmail.com>
2020-09-16 22:44:46 +0800

    debugging

--- grep_keywords.R
+++ grep_keywords.R
@@ -1,13 +1,14 @@
 library(data.table)
-all.files <- list.files(path = "C:/Users/tweet_keywords/tweets",pattern = ".csv")
+all.files <- list.files(path = "/Users/tweet_keywords/tweets",pattern = ".csv")
 l <- lapply(all.files, fread, sep=",")
-dt <- rbindlist(l)
+dt <- rbindlist(l, fill=TRUE) # Formatting
 tweets <- as.list(dt$tweet_text) 
-setwd("C:/Users/tweet_keywords")
+setwd("/Users/tweet_keywords")
 keywords <- readLines("keywords.txt") # Choose your own keywords
-setwd("C:/Users/tweet_keywords/outputs") # Change the destination of output files
+setwd("/Users/tweet_keywords/outputs") # Change the destination of output files
 for (i in 1:length(keywords)){ 
   tweets_keyword <- tweets[grep(keywords[i], tweets)]
   filename <- paste0("tweets_", length(tweets_keyword), "_", keywords[i], ".csv")
   fwrite(tweets_keyword, file = filename)
 }
+warning() # Show all warnings on the terminal
